2ebbfcb135d4d5bab60b5b26705f356bbbc2f838
ssartell <ssartell@rightpoint.com>
2017-11-27 20:04:41 -0600

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+var R = require('ramda');
+var fs = require('fs');
+var Stopwatch = require("node-stopwatch").Stopwatch;
+
+function pad(digit, width, char) {
+  char = char || '0';
+  digit = digit + '';
+  return digit.length >= width ? digit : new Array(width - digit.length + 1).join(char) + digit;
+}
+
+function run(day, part) {
+	day = pad(day, 2);
+	var input = fs.readFileSync('day' + day + '/input.txt', 'utf8');
+	var solution = require('./day' + day + '/part' + part);
+
+	var stopwatch = Stopwatch.create();
+	stopwatch.start();
+	
+	var answer = solution(input);
+	
+	stopwatch.stop();
+	
+	console.log('day ' + day + ', part ' + part + ' : ' + stopwatch.elapsed.seconds + 's');
+	console.log(answer);
+}
+
+run(1, 1);
+
+process.exit();
---
+++ utils.js
@@ -0,0 +1,7 @@
+var R = require('ramda');
+
+
+
+module.exports = {
+
+}
