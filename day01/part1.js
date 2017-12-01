9a59e841b1c78347f441c3c7f4aa7a5fc9fd2b4a
ssartell <ssartell@rightpoint.com>
2017-12-01 06:32:23 -0600

    day 1

--- day01/part1.js
+++ day01/part1.js
@@ -2,16 +2,15 @@ var R = require('ramda');
 
 var parseInput = R.pipe(R.trim, R.split(''), R.map(parseInt));
 
-var mod = (a, b) => a % b;
-var matchesNextDigit = R.curry((list, x, i) => list[mod(i + 1, list.length)] == x);
+var matchesNextDigit = (x, i, list) => list[(i + 1) % list.length] == x;
 var filterWithIndex = R.curry(function*(f, list) {
     for(var i = 0; i < list.length; i++) {
         var x = list[i];
-        if (f(x, i)) yield x;
+        if (f(x, i, list)) yield x;
     }
 });
 
-var getDigitsWithMatch = (list) => filterWithIndex(matchesNextDigit(list))(list);
+var getDigitsWithMatch = filterWithIndex(matchesNextDigit);
 
 var solution = R.pipe(parseInput, getDigitsWithMatch, R.sum);
 
--- day01/part2.js
+++ day01/part2.js
@@ -2,16 +2,15 @@ var R = require('ramda');
 
 var parseInput = R.pipe(R.trim, R.split(''), R.map(parseInt));
 
-var mod = (a, b) => a % b;
-var matchesHalfwayDigit = R.curry((list, x, i) => list[mod(i + list.length / 2, list.length)] == x);
+var matchesHalfwayDigit = (x, i, list) => list[(i + list.length / 2) % list.length] == x;
 var filterWithIndex = R.curry(function*(f, list) {
     for(var i = 0; i < list.length; i++) {
         var x = list[i];
-        if (f(x, i)) yield x;
+        if (f(x, i, list)) yield x;
     }
 });
 
-var getDigitsWithMatch = (list) => filterWithIndex(matchesHalfwayDigit(list))(list);
+var getDigitsWithMatch = filterWithIndex(matchesHalfwayDigit);
 
 var solution = R.pipe(parseInput, getDigitsWithMatch, R.sum);
 
