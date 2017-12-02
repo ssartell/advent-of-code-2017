2f90fd5c4d89030816f6630a4b263d9d954ae8f2
ssartell <ssartell@rightpoint.com>
2017-12-02 09:29:08 -0600

    switch to find after doing PR to package

--- day02/part2.js
+++ day02/part2.js
@@ -4,7 +4,7 @@ var C = require('js-combinatorics');
 var parseInput = R.pipe(R.split('\n'), R.map(R.pipe(R.split('\t'), R.map(parseInt))));
 
 var isDivisible = (a, b) => a != b && a % b === 0;
-var expandCombos = x => C.cartesianProduct(x, x).toArray();
+var expandCombos = x => C.cartesianProduct(x, x);
 var findDivision = R.pipe(expandCombos, R.find(R.apply(isDivisible)), R.apply(R.divide));
 
 var solution = R.pipe(parseInput, R.map(findDivision), R.sum);
