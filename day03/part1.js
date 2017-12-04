bb3f8333ec450c5345fbd47452b2d9a5494fdaa8
ssartell <ssartell@rightpoint.com>
2017-12-03 22:43:19 -0600

    day 3

--- day03/part1.js
+++ day03/part1.js
@@ -1,13 +1,14 @@
 var R = require('ramda');
 
-var dist = n => {
+var dist = (x, y) => Math.abs(x) + Math.abs(y);
+var getPos = n => {
     var sqr = Math.floor(Math.sqrt(n));
     var diff = n - Math.pow(sqr, 2);
     var x = Math.floor((sqr - 1) / 2) + Math.min(1, diff) - Math.max(diff - (sqr + 1), 0);
     var y = Math.floor(sqr / 2) - Math.min(Math.max(diff - 1, 0), sqr);
-    return Math.abs(x) + Math.abs(y);
+    return sqr % 2 === 1 ? [x, y] : [-x, -y];
 }
 
-var solution = R.pipe(parseInt, dist);
+var solution = R.pipe(parseInt, getPos, R.apply(dist));
 
 module.exports = solution;
--- day03/part2.js
+++ day03/part2.js
@@ -1,11 +1,9 @@
 var R = require('ramda');
 var C = require('js-combinatorics');
+var wu = require('wu');
 
-var oldVals = {};
-var neighbors = C.baseN([-1, 0, 1], 2).toArray();
-
+var neighbors = R.without([[0, 0]], C.baseN([-1, 0, 1], 2).toArray());
 var add = (a, b) => R.map(R.sum, R.zip(a, b));
-var getKey = pos => `${pos[0]},${pos[1]}`;
 var getPos = n => {
     var sqr = Math.floor(Math.sqrt(n));
     var diff = n - Math.pow(sqr, 2);
@@ -13,22 +11,23 @@ var getPos = n => {
     var y = Math.floor(sqr / 2) - Math.min(Math.max(diff - 1, 0), sqr);
     return sqr % 2 === 1 ? [x, y] : [-x, -y];
 }
-var neighborsSum = pos => R.sum(R.map(n => oldVals[getKey(add(pos, n))] || 0, neighbors));
 
-var spiral = n => {
+var spiral = function*() {
     var i = 1;
-    var val = 1;
-    
-    while (val < n) {
+    var grid = {};
+    var sumNeighbors = pos => R.sum(R.map(n => grid[add(pos, n)] || 0, neighbors));
+
+    while (true) {
         var pos = getPos(i);
-        var key = getKey(pos);
-        val = neighborsSum(pos) || 1;
-        oldVals[key] = val;
-        i++
+        var val = sumNeighbors(pos) || 1;
+        grid[pos] = val;
+        yield val;
+        i++;
     }
-    return val;
 }
 
-var solution = R.pipe(parseInt, spiral);
+var nextLargest = n =>  R.find(x => x > n, wu(spiral()));
+
+var solution = R.pipe(parseInt, nextLargest);
 
 module.exports = solution;
