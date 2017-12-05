002a5819e22a08f2220b4a8f6342ae7fe3144cc6
ssartell <ssartell@rightpoint.com>
2017-12-05 08:50:42 -0600

    day 5

---
+++ day05/part2.js
@@ -0,0 +1,20 @@
+var R = require('ramda');
+
+var parseInput = R.pipe(R.split('\n'), R.map(parseInt));
+
+var navigateMaze = maze => {
+    var i = 0;
+    var steps = 0;
+
+    while (true) {
+        if (i < 0 || i >= maze.length) return steps;
+        var delta = maze[i];
+        maze[i] += delta >= 3 ? -1 : 1;
+        i += delta;
+        steps++;
+    }
+};
+
+var solution = R.pipe(parseInput, navigateMaze);
+
+module.exports = solution;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var run = require('./scaffolding');
 
-run(4, 2);
+run(5, 1);
 
 process.exit();
