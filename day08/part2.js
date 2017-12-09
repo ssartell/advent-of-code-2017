26de241c4ff1cc198afe17897282135fea3d3a98
ssartell <ssartell@rightpoint.com>
2017-12-08 20:23:12 -0600

    day 8

---
+++ day08/part2.js
@@ -0,0 +1,37 @@
+var R = require('ramda');
+
+var debug = x => {debugger; return x;};
+
+var lineRegex = /(\w*) (\w*) (-?\d*) if (\w*) (\S*) (-?\d*)/;
+var readLine = R.pipe(R.match(lineRegex), R.tail, R.zipObj(['reg1', 'op', 'by', 'reg2', 'cond', 'val']), R.evolve({by: parseInt, val: parseInt}));
+var parseInput = R.pipe(R.split('\n'), R.map(readLine));
+
+var ops = {
+    '<': (a, b) => a < b,
+    '<=': (a, b) => a <= b,
+    '>': (a, b) => a > b,
+    '>=': (a, b) => a >= b,
+    '==': (a, b) => a == b,
+    '!=': (a, b) => a != b,
+    inc: (a, b) => a + b,
+    dec: (a, b) => a - b
+};
+
+var run = insts => {
+    var regs = {};
+    var max = -Infinity;
+    for(var i of insts) {
+        if (!R.has(i.reg1, regs)) regs[i.reg1] = 0;
+        if (!R.has(i.reg2, regs)) regs[i.reg2] = 0;
+        if (ops[i.cond](regs[i.reg2], i.val)) {
+            regs[i.reg1] = ops[i.op](regs[i.reg1], i.by);
+            if (regs[i.reg1] > max) max = regs[i.reg1];
+        }
+    }
+
+    return max;
+}
+
+var solution = R.pipe(parseInput, run);
+
+module.exports = solution;
--- tests.js
+++ tests.js
@@ -1,5 +1,2 @@
 var run = require('./scaffolding');
 
-run(8, 2);
-
-process.exit();
