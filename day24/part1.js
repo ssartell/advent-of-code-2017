125c81b0f1174eb191c05fb716f4f5b51fc464b6
ssartell <ssartell@rightpoint.com>
2017-12-24 08:35:59 -0600

    day 24

--- day24/part1.js
+++ day24/part1.js
@@ -4,16 +4,12 @@ var Queue = require('mnemonist/queue');
 var readLine = R.pipe(R.trim, R.split('/'), R.map(parseInt), R.zipObj(['l', 'r']));
 var parseInput = R.pipe(R.trim, R.split('\n'), R.map(readLine));
 
-var maxBridge = (r, components) => {
+var maxBridge = R.curry((r, components) => {
     var validComponents = R.filter(x => x.l === r || x.r === r, components);
     if (validComponents.length === 0) return 0;
     return R.reduce((a, x) => R.max(a, x.l + x.r + maxBridge(x.l === r ? x.r : x.l, R.without([x], components))), 0, validComponents)
-}
+});
 
-var run = components => {
-    return maxBridge(0, components);
-}
-
-var solution = R.pipe(parseInput, run);
+var solution = R.pipe(parseInput, maxBridge(0));
 
 module.exports = solution;
--- day24/part2.js
+++ day24/part2.js
@@ -4,7 +4,7 @@ var Queue = require('mnemonist/queue');
 var readLine = R.pipe(R.trim, R.split('/'), R.map(parseInt), R.zipObj(['l', 'r']));
 var parseInput = R.pipe(R.trim, R.split('\n'), R.map(readLine));
 
-var maxBridge = (state, components) => {
+var maxBridge = R.curry((state, components) => {
     var validComponents = R.filter(x => x.l === state.r || x.r === state.r, components);
     if (validComponents.length === 0) return state;
     return R.reduce((a, x) => {
@@ -16,12 +16,8 @@ var maxBridge = (state, components) => {
         var max = maxBridge(newState, R.without([x], components));
         return (max.length === a.length) && (max.str > a.str) || (max.length > a.length) ? max : a;
     }, {str: 0, length: 0}, validComponents)
-}
+});
 
-var run = components => {
-    return maxBridge({str: 0, length: 0, r: 0}, components).str;
-}
-
-var solution = R.pipe(parseInput, run);
+var solution = R.pipe(parseInput, maxBridge({str: 0, length: 0, r: 0}), R.prop('str'));
 
 module.exports = solution;
